2dc873dd44e38c5f07c34f3335b00e4694d2122d
Minosu2a <64777323+Minosu2a@users.noreply.github.com>
2021-09-22 14:45:49 +0200

    Lot of Stuff (not really just too lazy to find a name)

---
+++ EtageReception/Assets/GrabDetection.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabDetection : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private List<GameObject> _grabRangeObjects = null;
+    #endregion Fields
+
+
+    #region Properties
+    public List<GameObject> GrabRangeObject
+    {
+        get
+        {
+            return _grabRangeObjects;
+        }
+    }
+    #endregion Properties
+
+    void Start()
+    {
+        _grabRangeObjects = new List<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Items" || other.tag == "Key")
+        {
+            _grabRangeObjects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Items" || other.tag == "Key")
+        {
+            _grabRangeObjects.Remove(other.gameObject);
+        }
+    }
+}
---
+++ EtageReception/Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    #region Fields
+    [Header("Movement")]
+    [SerializeField] private Rigidbody _rb = null;
+    [SerializeField] private float _walkSpeed = 250f;
+    [SerializeField] private float _sprintSpeed = 500f;
+    private bool _isMoving = false;
+
+    [Header("Look")]
+    private Vector3 _posMouse = Vector3.zero;
+    private Vector3 _mouseRotdir = Vector3.zero;
+
+
+     [SerializeField] private GrabDetection _grabDetection = null;
+     [SerializeField] private GameObject _centerGrabPoint = null;
+
+
+    #endregion Fields
+
+
+    #region Properties
+
+    public Rigidbody Rb => _rb;
+
+    public Vector3 PosMouse => _posMouse;
+
+    
+    #endregion Properties
+
+
+    #region Methods
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        CharacterManager.Instance.CharacterController = this;    
+    }
+
+    private void Update()
+    {
+      
+
+
+        Walk();
+
+        if(_rb.velocity != Vector3.zero)
+        {
+            _isMoving = true;
+        }
+        else
+        {
+            _isMoving = false; 
+        }
+    }
+
+
+    private void Look()
+    {
+        transform.forward = _rb.velocity;
+    }
+
+    public void Walk()
+    {
+        _rb.velocity = InputManager.Instance.MoveDir * _walkSpeed;
+    }
+
+    public void Sprint()
+    {
+        _rb.velocity = InputManager.Instance.MoveDir * _sprintSpeed;
+    }
+
+    public void Grab()
+    {
+        if(_grabDetection.GrabRangeObject.Count >= 0)
+        {
+            for (int i =0; i < _grabDetection.GrabRangeObject.Count; i++ )
+            {
+
+                float smallerDistance = 0;
+                int integerOfCloserObject = 0;
+
+                float dist = Vector3.Distance(_grabDetection.GrabRangeObject[i].transform.position, _centerGrabPoint.transform.position);
+
+                if(i == 0)
+                {
+                    smallerDistance = dist;
+                    integerOfCloserObject = i;
+                }
+                else if(dist < smallerDistance)
+                {
+                    smallerDistance = dist;
+                    integerOfCloserObject = i;
+                }
+
+                FixedJoint fj = _grabDetection.GrabRangeObject[i].AddComponent<FixedJoint>();
+                fj.connectedBody = _rb;
+
+                // _centerGrabPoint
+                // _grabDetection.GrabRangeObject[i].transform.position;
+            }
+
+        }
+
+    }
+
+    public void Jump()
+    {
+
+    }
+
+
+
+    #endregion Methods
+
+
+
+}
