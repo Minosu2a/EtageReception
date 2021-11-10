6f39e9b3679ca4a3c0b7859ed97180615def8864
ClemCa <perso.catorc@gmail.com>
2021-11-10 12:32:42 +0100

    finished work on animation base

--- EtageReception/Assets/Ragdoll/RagdollController.cs
+++ EtageReception/Assets/Ragdoll/RagdollController.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 using System.Linq;
 using ClemCAddons.Utilities;
 using System;
+using ClemCAddons;
 
 public class RagdollController : MonoBehaviour
 {
     [SerializeField] private float _speed = 1;
     [SerializeField] private float _jumpStrength = 1;
     [SerializeField] private AnimationInfo _animations;
+    private Animator _animator;
     private List<GameObject> joints = new List<GameObject>();
-    private List<KeyValuePair<int, AnimationInfo.InfoAnimation>> _currentAnimations = new List<KeyValuePair<int, AnimationInfo.InfoAnimation>>();
+    private List<KeyValuePair<int, KeyValuePair<float, AnimationInfo.InfoAnimation>>> _currentAnimations = new List<KeyValuePair<int, KeyValuePair<float, AnimationInfo.InfoAnimation>>>();
 
     private bool isRunningAnimation = true;
 
@@ -19,6 +21,7 @@ public class RagdollController : MonoBehaviour
     {
         var r = FindObjectsOfType<CopyMotion>();
         joints = r.Select(t => t.gameObject).ToList();
+        _animator = transform.FindDeep("AnimationBody").GetComponent<Animator>();
     }
 
     void Update()
@@ -28,35 +31,35 @@ public class RagdollController : MonoBehaviour
         {
             input = true;
             AddForceOnJoint("Root",transform.right * _speed * Time.deltaTime * 1000);
-            AddForceOnJoint("LegL", transform.right * _speed * Time.deltaTime * 1000);
-            AddForceOnJoint("LegR", transform.right * _speed * Time.deltaTime * 1000);
+            AddForceOnJoint("LeftLeg", transform.right * _speed * Time.deltaTime * 1000);
+            AddForceOnJoint("RightLeg", transform.right * _speed * Time.deltaTime * 1000);
         }
         if (Input.GetKey(KeyCode.S))
         {
             input = true;
             AddForceOnJoint("Root", transform.right * _speed * Time.deltaTime * -1000);
-            AddForceOnJoint("LegL", transform.right * _speed * Time.deltaTime * -1000);
-            AddForceOnJoint("LegR", transform.right * _speed * Time.deltaTime * -1000);
+            AddForceOnJoint("LeftLeg", transform.right * _speed * Time.deltaTime * -1000);
+            AddForceOnJoint("RightLeg", transform.right * _speed * Time.deltaTime * -1000);
         }
         if (Input.GetKey(KeyCode.A))
         {
             input = true;
             AddForceOnJoint("Root", transform.forward * _speed * Time.deltaTime * 1000);
-            AddForceOnJoint("LegL", transform.forward * _speed * Time.deltaTime * 1000);
-            AddForceOnJoint("LegR", transform.forward * _speed * Time.deltaTime * 1000);
+            AddForceOnJoint("LeftLeg", transform.forward * _speed * Time.deltaTime * 1000);
+            AddForceOnJoint("RightLeg", transform.forward * _speed * Time.deltaTime * 1000);
         }
         if (Input.GetKey(KeyCode.D))
         {
             input = true;
             AddForceOnJoint("Root", transform.forward * _speed * Time.deltaTime * -1000);
-            AddForceOnJoint("LegL", transform.forward * _speed * Time.deltaTime * -1000);
-            AddForceOnJoint("LegR", transform.forward * _speed * Time.deltaTime * -1000);
+            AddForceOnJoint("LeftLeg", transform.forward * _speed * Time.deltaTime * -1000);
+            AddForceOnJoint("RightLeg", transform.forward * _speed * Time.deltaTime * -1000);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             AddForceOnJoint("Root", transform.up * _jumpStrength * 1000);
-            AddForceOnJoint("ArmL", transform.up * _jumpStrength * 1000);
-            AddForceOnJoint("ArmR", transform.up * _jumpStrength * 1000);
+            AddForceOnJoint("LeftArm", transform.up * _jumpStrength * 1000);
+            AddForceOnJoint("RightArm", transform.up * _jumpStrength * 1000);
         }
         if(!isRunningAnimation && input)
         {
@@ -83,7 +86,7 @@ public class RagdollController : MonoBehaviour
         }
     }
 
-    public void StartAnimation(AnimationInfo.InfoAnimation animation)
+    public void StartAnimation(AnimationInfo.InfoAnimation animation, bool stopAfterTime = false)
     {
         var r = joints.FindAll(t => animation.UsedJoints.Contains(t.GetComponent<ConfigurableJoint>())).Select(t => t.GetComponent<CopyMotion>()).ToArray();
         if (r != null)
@@ -91,19 +94,28 @@ public class RagdollController : MonoBehaviour
                 CopyJointAnimation(r[i]);
         var rand = new System.Random();
         int t = rand.Next();
-        _currentAnimations.Add(new KeyValuePair<int,AnimationInfo.InfoAnimation>(t, animation));
-        ClemCAddons.Utilities.Timer.StartTimer(t, Mathf.RoundToInt(animation.Anim.length * 1000), StopCurrentAnimation, false);
+        _animator.SetTrigger(animation.Name);
+        for (int i = 0; i < _currentAnimations.Count; i++)
+            _currentAnimations[i] = new KeyValuePair<int, KeyValuePair<float, AnimationInfo.InfoAnimation>>(_currentAnimations[i].Key, new KeyValuePair<float, AnimationInfo.InfoAnimation>(_currentAnimations[i].Value.Key - Mathf.RoundToInt(animation.Anim.length * 1000), _currentAnimations[i].Value.Value));
+        _currentAnimations.Add(new KeyValuePair<int, KeyValuePair<float, AnimationInfo.InfoAnimation>>(t, new KeyValuePair<float, AnimationInfo.InfoAnimation>(Mathf.RoundToInt(animation.Anim.length * 1000), animation)));
+        if (!stopAfterTime)
+        {
+            ClemCAddons.Utilities.Timer.StartTimer(t, Mathf.RoundToInt(animation.Anim.length * 1000), StopCurrentAnimation, false);
+        }
     }
 
     public void StopCurrentAnimation()
     {
         if(_currentAnimations.Count() > 0)
         {
-            ClemCAddons.Utilities.Timer.StopTimer(_currentAnimations[0].Key);
-            var r = joints.FindAll(t => _currentAnimations[0].Value.UsedJoints.Contains(t.GetComponent<ConfigurableJoint>())).Select(t => t.GetComponent<CopyMotion>()).ToArray();
+            var res = _currentAnimations.Select(t => t.Value.Key).Min();
+            var v = _currentAnimations.Find(e => e.Value.Key == res);
+            ClemCAddons.Utilities.Timer.StopTimer(v.Key);
+            var r = joints.FindAll(t => _currentAnimations[0].Value.Value.UsedJoints.Contains(t.GetComponent<ConfigurableJoint>())).Select(t => t.GetComponent<CopyMotion>()).ToArray();
             if (r != null)
                 for (int i = 0; i < r.Length; i++)
                     StopCopyingJointAnimation(r[i]);
+            _animator.SetTrigger(_currentAnimations[0].Value.Value.Name+"End");
             _currentAnimations.RemoveAt(0);
         }
     }
@@ -120,7 +132,7 @@ public class RagdollController : MonoBehaviour
 
     public void AddForceOnJoint(string joint, Vector3 force)
     {
-        transform.Find(joint).GetComponent<Rigidbody>().AddForce(force);
+        transform.FindDeep(joint).GetComponent<Rigidbody>().AddForce(force);
     }
     public void AddForceOnJoint(CopyMotion joint, Vector3 force)
     {
