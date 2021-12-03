da9796a3827f21f784b1d0be439b7ea3eee8f301
Minosu2a <64777323+Minosu2a@users.noreply.github.com>
2021-12-03 10:27:02 +0100

    Camera behaviour not working (testing other directions)

--- EtageReception/Assets/Scripts/Character/CameraBehaviour.cs
+++ EtageReception/Assets/Scripts/Character/CameraBehaviour.cs
@@ -77,99 +77,131 @@ public class CameraBehaviour : MonoBehaviour
 
     void Start()
     {
-        _defaultQuaternion.eulerAngles = new Vector3(20, 0, 0);
+        _defaultQuaternion.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 
     void Update()
     {
-   
+        Transform characterTransform = CharacterManager.Instance.CharacterController.transform.FindDeep("Root");
 
+        //----------------- CONFIG 1 ----------------------//
+
+        /*
 
         //CACLUL DE L'OBJECTIF DE LA CAMERA X
-        if(CharacterManager.Instance.CharacterController.transform.GetChild(0).GetChild(0).transform.position.x > _centerOfMap.transform.position.x)
+        if (characterTransform.position.x > _centerOfMap.transform.position.x)
         {
             //CALCUL DE POURCENTAGE POUR LA POSITION X (Droit)
-            _xRightPercentage = Mathf.InverseLerp(_centerOfMap.position.x, _rightMaximumXPlayerPos.position.x, CharacterManager.Instance.CharacterController.transform.GetChild(0).GetChild(0).transform.position.x);
+            _xRightPercentage = Mathf.InverseLerp(_centerOfMap.position.x, _rightMaximumXPlayerPos.position.x, characterTransform.position.x);
             _isOnRightSide = true;
+            Debug.Log("Test Step 1 Right");
+
 
             _rightQuaternion = transform.rotation;
-            _rightQuaternion.eulerAngles = new Vector3(_xConstantRotation, _rightRotationMaxLooking, 0);
+            _rightQuaternion.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, _rightRotationMaxLooking, 0);
         }
         else
         {
             //CALCUL DE POURCENTAGE POUR LA POSITION X (Gauche)
-            _xLeftPercentage = Mathf.InverseLerp(_centerOfMap.position.x, _leftMaximumXPlayerPos.position.x, CharacterManager.Instance.CharacterController.transform.GetChild(0).GetChild(0).transform.position.x);
+            _xLeftPercentage = Mathf.InverseLerp(_centerOfMap.position.x, _leftMaximumXPlayerPos.position.x, characterTransform.position.x);
             _isOnRightSide = false;
-            
+            Debug.Log("Test Step 1 Left");
+
 
             //CALCUL DE LA ROTATION QUAND ON EST SUR LA GAUCHE
             _leftQuaternion = transform.rotation;
-            _leftQuaternion.eulerAngles = new Vector3(_xConstantRotation, _leftRotationMaxLooking, 0);
+            _leftQuaternion.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, _leftRotationMaxLooking, 0);
         }
 
 
 
         //DISTANCE Z DE LA CAMERA (ZconstanstDistance)
-        float zPosition = CharacterManager.Instance.CharacterController.transform.GetChild(0).GetChild(0).transform.position.z - _zConstantDistance;
+        //float zPosition = characterTransform.position.z - _zConstantDistance;
 
-        Vector3 leForwardEHEH = new Vector3(this.transform.position.x, CharacterManager.Instance.CharacterController.transform.GetChild(0).GetChild(0).transform.position.x);
+        //Vector3 leForwardEHEH = new Vector3(this.transform.position.x, characterTransform.position.x);
+        Debug.Log("Test Step 2");
 
 
 
         //CALCUL DE LA POSITION DE LA CAMERA
-        if(_isOnRightSide == true)
+        if (_isOnRightSide == true)
         {
             float XPos = Mathf.Lerp(_centerOfMap.position.x, _leftMaximumXCamPos.position.x, _xRightPercentage);
-            transform.position = new Vector3(XPos, _yConstantDistance, zPosition);
+            transform.position = new Vector3(XPos, _yConstantDistance, transform.position.z);
 
             transform.rotation = Quaternion.Lerp(_defaultQuaternion, _rightQuaternion, _xRightPercentage);
+            Debug.Log("Test Step 3 Right");
+
         }
         else
         {
             float XPos = Mathf.Lerp(_centerOfMap.position.x, _rightMaximumXCamPos.position.x, _xLeftPercentage);
-            transform.position = new Vector3(XPos, _yConstantDistance, zPosition);
+            transform.position = new Vector3(XPos, _yConstantDistance, transform.position.z);
 
             transform.rotation = Quaternion.Lerp(_defaultQuaternion, _leftQuaternion, _xLeftPercentage);
+
+            Debug.Log("Test Step 3 Left");
         }
 
-        //REGARDER LE JOUEUR
-        //PLUS LA ROTATION EN FONCTION DU X
-        //transform.forward = leForwardEHEH;
+        */
+
+        //----------------- CONFIG 2 ----------------------//
 
 
-        //CALCULER L'OBJECTIF (Check le pourcentage de la position du player entre le centerOfMap et le maximumPlayerPos)
-        //APPLIQUER CE MÊME POURCENTAGE SUR LA POSITION DE LA CAMERA AVEC UN LERP (MAXIMUMCAMPOSRIGHT, MAXIMUMCAMPOSLEFT, pourcentage trouvé)
+        //CACLUL DE L'OBJECTIF DE LA CAMERA X
+        if (characterTransform.position.x > _centerOfMap.transform.position.x)
+        {
+            //CALCUL DE POURCENTAGE POUR LA POSITION X (Droit)
+            _xRightPercentage = Mathf.InverseLerp(_centerOfMap.position.x, _rightMaximumXPlayerPos.position.x, characterTransform.position.x);
+            _isOnRightSide = true;
+            Debug.Log("Test Step 1 Right");
+
+
+            _rightQuaternion = transform.rotation;
+            _rightQuaternion.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, _rightRotationMaxLooking, 0);
+        }
+        else
+        {
+            //CALCUL DE POURCENTAGE POUR LA POSITION X (Gauche)
+            _xLeftPercentage = Mathf.InverseLerp(_centerOfMap.position.x, _leftMaximumXPlayerPos.position.x, characterTransform.position.x);
+            _isOnRightSide = false;
+            Debug.Log("Test Step 1 Left");
+
+
+            //CALCUL DE LA ROTATION QUAND ON EST SUR LA GAUCHE
+            _leftQuaternion = transform.rotation;
+            _leftQuaternion.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, _leftRotationMaxLooking, 0);
+        }
 
 
 
+        //DISTANCE Z DE LA CAMERA (ZconstanstDistance)
+        //float zPosition = characterTransform.position.z - _zConstantDistance;
 
-
-        /*
-        //transform.LookAt(CharacterManager.Instance.CharacterController.transform.GetChild(0).GetChild(0));
-        //MOVE THE Z POS
-       float ZPlayerPos = Mathf.InverseLerp(_wallFront.position.z, _wallBack.position.z, CharacterManager.Instance.CharacterController.transform.FindDeep("Root").position.z);
-       float ZPosToUpdate = Mathf.Lerp(_maximumZ.position.z, _minimumZ.position.z, ZPlayerPos);
+        //Vector3 leForwardEHEH = new Vector3(this.transform.position.x, characterTransform.position.x);
+        Debug.Log("Test Step 2");
 
 
 
-        //MOVE THE X POS
-        float XPlayerPos = Mathf.InverseLerp(_wallRight.position.x, _wallLeft.position.x, CharacterManager.Instance.CharacterController.transform.FindDeep("Root").position.x);
+        //CALCUL DE LA POSITION DE LA CAMERA
+        if (_isOnRightSide == true)
+        {
+            float XPos = Mathf.Lerp(_centerOfMap.position.x, _rightMaximumXCamPos.position.x, _xRightPercentage);
+            transform.position = new Vector3(XPos, _yConstantDistance, transform.position.z);
 
-        float XPosToUpdate = Mathf.Lerp(_maximumX.position.x, _minimumX.position.x, XPlayerPos);
+            transform.rotation = Quaternion.Lerp(_defaultQuaternion, _rightQuaternion, _xRightPercentage);
+            Debug.Log("Test Step 3 Right");
 
+        }
+        else
+        {
+            float XPos = Mathf.Lerp(_centerOfMap.position.x, _leftMaximumXCamPos.position.x, _xLeftPercentage);
+            transform.position = new Vector3(XPos, _yConstantDistance, transform.position.z);
 
-        float YPlayerPos = Mathf.Lerp(_zoomFront, _zoomBack, XPlayerPos);   
+            transform.rotation = Quaternion.Lerp(_defaultQuaternion, _leftQuaternion, _xLeftPercentage);
 
-
-        Vector3 objPos = new Vector3(XPosToUpdate, YPlayerPos, ZPosToUpdate);
-        transform.position = Vector3.Lerp(transform.position, objPos, Time.smoothDeltaTime / _movementSmoothValue);
-
-
-        //ROTATION ON THE CORNER OF THE ROOM
-        Quaternion rotCam = Quaternion.Lerp(_rotationLeft, _rotationRight, XPlayerPos); 
-
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotCam, Time.smoothDeltaTime / _rotationSmoothValue);*/
-
+            Debug.Log("Test Step 3 Left");
+        }
 
 
     }
